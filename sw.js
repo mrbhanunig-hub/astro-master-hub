0ade1bfda1b48956b8d65990c194580a57dcc544
mrbhanunig-hub <mr.bhanunig@gmail.com>
2026-09-12 19:11:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'cosmic-hub-v16';
+const CACHE_NAME = 'cosmic-hub-v17';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
   './icon.svg',
+  './fortuna_point.html',
   './gulbarga_prashna_app.html',
   './aaj-ka-rashifal.html',
   './Muhurat_Vichar.html',
